7d1735bd77ee3893bcc40969c4a670613cbd4a08
ubuntubox <shayansolutions@gmail.com>
2013-06-09 15:49:28 +0500

    adding code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '0ceae062a7e6b68e10d2ce8518cbfdd7248600c3b5dd37831337717faecb31edfc9c94d1d76a59f00fa8dcf2b44e74e6a38938a3677ab7c635cfd883281dc39a'
